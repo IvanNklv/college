f0349e4b35bf4e00451e5522926015b29310262a
IvanNklv <nikolaevivan229@gmail.com>
2025-02-01 20:42:55 +0200

    Add files via upload

---
+++ Bazoviosnovy1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+	std::cout << "<head>\n";
+	std::cout << "\t<h1>Hello world</h1>\n";
+	std::cout << "</head>\n";
+		return 0;
+}
---
+++ zminni.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+int main()
+{
+	int num5;
+	int num6;
+	std::cout << "enter two numbers(not 0)-";
+	std::cin >> num5 >> num6;
+	int myAge = 18;
+	int num1 {25};
+	int num2 = 2;
+	int num3 = 52;
+	int num4 = 0;
+	char ivn = 'a';
+	const double PI = 3.14;
+	std::cout << myAge << std::endl;
+	std::cout << "myAge is " << myAge << std::endl;
+	std::cout << "((num 1 + num2 + num3 + num4) * PI + num5)/num6 = " << ((num1 + num2 + num3 + num4) * PI + num5)/num6 << ivn << std::endl;
+	
+
+
+
+
+
+
+
+	return 0;
+
+}
